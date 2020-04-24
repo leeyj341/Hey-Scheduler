7d88708d388b4516a483537a8649da0a0c8ca9dd
ParkNuri <qkrsnrl@naver.com>
2020-04-24 17:27:48 +0900

    first upload project (with layout files)

---
+++ HeySched/app/src/main/java/exam/day03/view/heysched/ChatActivity.java
@@ -0,0 +1,14 @@
+package exam.day03.view.heysched;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_chat);
+    }
+}
